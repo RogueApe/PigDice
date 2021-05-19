fec4d8d7500c6bd8b342dcffdcd1fd7f5225a96c
VP <78648534+RogueApe@users.noreply.github.com>
2021-05-20 03:42:37 +0800

    Adjust the game rules to 100 points

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playGame = false;
 
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
